2e2a37e1bcc3057bf46abcf7cc2319db090ddf5e
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-08-27 01:45:15 +0300

    Task 1: Calculate statistical measures

---
+++ statistical-measures-calculator/main.py
@@ -0,0 +1,23 @@
+def calculate(arr):
+
+  matrix = np.array(test_case).reshape(3, 3)
+
+  axis_variants = [0, 1, None]
+  result = {
+    'mean': [],
+    'variance': [],
+    'standard deviation': [],
+    'max': [],
+    'min': [],
+    'sum': []
+  }
+
+  for ax in axis_variants:
+    result['mean'].append(matrix.mean(axis=ax))
+    result['variance'].append(matrix.var(axis=ax))
+    result['standard deviation'].append(matrix.std(axis=ax))
+    result['max'].append(matrix.max(axis=ax))
+    result['min'].append(matrix.min(axis=ax))
+    result['sum'].append(matrix.sum(axis=ax))
+
+  return result
